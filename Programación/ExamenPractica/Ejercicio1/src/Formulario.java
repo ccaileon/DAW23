e60dd7e76e2980ce16db0400ce90e7ff81d71251
Caileon <109900171+Schekya@users.noreply.github.com>
2024-05-16 21:41:40 +0200

    update

--- Programación/ExamenPractica/Ejercicio1/src/Formulario.java
+++ Programación/ExamenPractica/Ejercicio1/src/Formulario.java
@@ -73,6 +73,7 @@ public class Formulario {
         System.out.println("Nombre: " + nombre);
         System.out.println("Apellidos: " + apellidos);
         System.out.println("DNI: " + dni);
+        System.out.println("Email: " + email);
 
     }
 
